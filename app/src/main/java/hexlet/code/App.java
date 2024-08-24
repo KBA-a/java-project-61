f1cff79b680b166123d79f83ad3eb245acdd6a04
KBA-a <mackh2121@gmail.com>
2024-08-24 19:36:45 +0500

    implementation game menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,11 +1,21 @@
 package hexlet.code;
 
 
+import java.util.Scanner;
 
 class App {
 
     public static void main(String[] args) {
-        Cli.userGreeting();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n0 - Exit");
+        int choiceOfGame = input.nextInt();
+        switch(choiceOfGame) {
+            case 1: {
+                Cli.userGreeting();
+            }
+            case 0:
+                break;
+            }
     }
 
 }
