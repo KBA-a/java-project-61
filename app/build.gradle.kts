26f25f62eefb1d41cd53c8605a381596554a0237
KBA-a <mackh2121@gmail.com>
2024-08-25 16:54:52 +0500

    add commons-lang in dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,8 @@ application {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation ("org.apache.commons:commons-lang3:3.16.0")
+
 }
 
 tasks.test {
