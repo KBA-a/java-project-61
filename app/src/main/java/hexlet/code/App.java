1114ede8c9fa47acb3eaa2ef759416998a38bf27
KBA-a <mackh2121@gmail.com>
2024-08-25 16:58:31 +0500

    add new game in menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,15 +7,20 @@ class App {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n0 - Exit");
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit");
         int choiceOfGame = input.nextInt();
-        switch(choiceOfGame) {
-            case 1: {
-                Cli.userGreeting();
-            }
+        switch (choiceOfGame) {
+            case 1:
+                System.out.println("Welcome to the Brain Games!");
+                System.out.print("May I have your name? ");
+                String userName = input.nextLine();
+                System.out.println("Hello, " + userName + "!");
+                break;
+            case 2:
+                Games.even();
+                break;
             case 0:
                 break;
-            }
+        }
     }
-
 }
