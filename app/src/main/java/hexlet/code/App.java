37b09f789b6047a9104e5e91cef09d8cf49172eb
KBA-a <mackh2121@gmail.com>
2024-08-29 03:12:44 +0500

    add new game in menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,7 +7,8 @@ class App {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit");
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n" +
+                "3 - Calc\n0 - Exit");
         int choiceOfGame = input.nextInt();
         switch (choiceOfGame) {
             case 1:
@@ -19,8 +20,12 @@ class App {
             case 2:
                 Games.even();
                 break;
+            case 3:
+                Games.Calc();
             case 0:
                 break;
+            default:
+                System.out.println("You entered something wrong. Try again!");
         }
     }
 }
