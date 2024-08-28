60bdf1057abbeeb358653296584b33d5eaaa0426
KBA-a <mackh2121@gmail.com>
2024-08-29 03:15:52 +0500

    add library "commons-lang3" and Engine

---
+++ app/src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,35 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Engine {
+
+    public static void gameLogic (String requirement,String[] questions, String[] rightAnswers) {
+
+        byte counterRightAnswer = 0;
+        Scanner input = new Scanner(System.in);
+        String usersAnswer;
+
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String userName = input.nextLine();
+        System.out.println("Hello, " + userName + "!");
+
+        System.out.println(requirement);
+        do {
+            System.out.print("Question: " + questions[counterRightAnswer] + "\nYour answer: ");
+            usersAnswer = input.nextLine();
+            if (usersAnswer.equalsIgnoreCase(rightAnswers[counterRightAnswer])) {
+                System.out.println("Correct!");
+                counterRightAnswer++;
+            } else {
+                System.out.println("'" + usersAnswer + "' " + "is wrong answer ;(. Correct answer was "
+                        + "'" + rightAnswers[counterRightAnswer] + "' " + "\nLet's try again, " + userName + "!");
+                return;
+            }
+
+        } while (counterRightAnswer < 3);
+        System.out.println("Congratulation, " + userName + "!");
+    }
+
+}
