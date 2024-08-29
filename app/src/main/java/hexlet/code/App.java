cba48d37ff3b0bcc7ebe69f7cf4cee98ef963e1b
KBA-a <mackh2121@gmail.com>
2024-08-29 21:18:32 +0500

    add game "gsd" in gamemenu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,7 +7,8 @@ class App {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n0 - Exit");
+        System.out.println("Please enter the game number and press Enter." +
+                "1 - Greet\n2 - Even\n3 - Calc\n4 - GSD\n0 - Exit");
         int choiceOfGame = input.nextInt();
         switch (choiceOfGame) {
             case 1:
@@ -23,7 +24,8 @@ class App {
                 Games.calc();
                 break;
             case 4:
-
+                Games.GSD();
+                break;
             case 0:
                 break;
             default:
