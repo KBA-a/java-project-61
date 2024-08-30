1ed19e392f6c14350b025c96bd6eb7b020875740
KBA-a <mackh2121@gmail.com>
2024-08-30 22:46:23 +0500

    add new game "progression"

--- app/src/main/java/hexlet/code/Games.java
+++ app/src/main/java/hexlet/code/Games.java
@@ -60,15 +60,15 @@ public class Games {
 
     public static void GSD() {
 
-        String[] question = new String[3];
-        String[] rightAnswer = new String[3];
+        String[] questions = new String[3];
+        String[] rightAnswers = new String[3];
 
         for(int i = 0; i < 3; i++) {
 
             int firstNumber = RandomUtils.nextInt(0, 100);
             int secondNumber = RandomUtils.nextInt(0, 100);
 
-            question[i] = firstNumber + " " + secondNumber;
+            questions[i] = firstNumber + " " + secondNumber;
             while (firstNumber != 0 && secondNumber != 0) {
                 if (firstNumber > secondNumber) {
                     firstNumber = firstNumber % secondNumber;
@@ -76,8 +76,40 @@ public class Games {
                     secondNumber = secondNumber % firstNumber;
                 }
             }
-            rightAnswer[i] = Math.max(firstNumber, secondNumber) + "";
+            rightAnswers[i] = Math.max(firstNumber, secondNumber) + "";
         }
-        Engine.gameLogic("Find the greatest common divisor of given numbers.", question , rightAnswer);
+        Engine.gameLogic("Find the greatest common divisor of given numbers.", questions , rightAnswers);
+    }
+
+    public static void progression() {
+
+
+        String[] rightAnswers = new String[3];
+        String[] questions = new String[3];
+
+        for (int i = 0; i < 3; i++) {
+            int lengthProgression = RandomUtils.nextInt(8, 15);
+            int stepProgression = RandomUtils.nextInt(1, 10);
+            int missingElement = RandomUtils.nextInt(0, lengthProgression - 1);
+            int elementProgression = RandomUtils.nextInt(0, 47);
+
+            if (missingElement == 0) {
+                questions[i] = "..";
+                rightAnswers[i] = elementProgression + "";
+            }   else {
+                questions[i] = elementProgression + "";
+            }
+            for (int j = 1; j < lengthProgression; j++) {
+                elementProgression += stepProgression;
+                if (missingElement == j) {
+                    rightAnswers[i] = elementProgression + "";
+                    questions[i] = questions[i] + " ..";
+                } else {
+                    questions[i] = questions[i] + " " + elementProgression;
+                }
+            }
+
+        }
+        Engine.gameLogic("What number is missing in the progression?", questions, rightAnswers);
     }
 }
