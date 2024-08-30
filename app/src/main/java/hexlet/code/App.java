a1a96c7fabbf27aecc7b5c5ecdc00267896ab5f2
KBA-a <mackh2121@gmail.com>
2024-08-30 22:45:39 +0500

    add new game "progression" in menu and fix menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,8 +7,8 @@ class App {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter." +
-                "1 - Greet\n2 - Even\n3 - Calc\n4 - GSD\n0 - Exit");
+        System.out.print("Please enter the game number and press Enter." +
+                "\n1 - Greet\n2 - Even\n3 - Calc\n4 - GSD\n5 - Progression\n0 - Exit\nYour choice: ");
         int choiceOfGame = input.nextInt();
         switch (choiceOfGame) {
             case 1:
@@ -25,6 +25,9 @@ class App {
                 break;
             case 4:
                 Games.GSD();
+                break;
+            case 5:
+                Games.progression();;
                 break;
             case 0:
                 break;
