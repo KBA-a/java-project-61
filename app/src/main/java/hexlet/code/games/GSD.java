a6d4f013aae07306fd926889314d1a80b26f3fde
KBA-a <mackh2121@gmail.com>
2024-09-02 21:40:32 +0500

    fix problem checking codeClimate

--- app/src/main/java/hexlet/code/games/GSD.java
+++ app/src/main/java/hexlet/code/games/GSD.java
@@ -17,15 +17,19 @@ public class GSD {
             int secondNumber = RandomUtils.nextInt(0, maxNumber);
 
             questions[i] = firstNumber + " " + secondNumber;
-            while (firstNumber != 0 && secondNumber != 0) {
-                if (firstNumber > secondNumber) {
-                    firstNumber = firstNumber % secondNumber;
-                } else {
-                    secondNumber = secondNumber % firstNumber;
-                }
-            }
-            rightAnswers[i] = Math.max(firstNumber, secondNumber) + "";
+            rightAnswers[i] = searchingGSD(firstNumber, secondNumber);
         }
         Engine.gameLogic("Find the greatest common divisor of given numbers.", questions, rightAnswers);
+    }
+
+    public static String searchingGSD(int firstNumber, int secondNumber) {
+        while (firstNumber != 0 && secondNumber != 0) {
+            if (firstNumber > secondNumber) {
+                firstNumber = firstNumber % secondNumber;
+            } else {
+                secondNumber = secondNumber % firstNumber;
+            }
+        }
+        return Math.max(firstNumber, secondNumber) + "";
     }
 }
