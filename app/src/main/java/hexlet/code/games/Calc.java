b29fcf84305f08f514655ba6c33ca86c2aecf7bc
KBA-a <mackh2121@gmail.com>
2024-09-02 21:20:59 +0500

    fix problem with check codeClimate

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -6,38 +6,46 @@ import org.apache.commons.lang3.RandomUtils;
 public class Calc {
     public static void calc() {
 
-        int firstNumber;
-        int secondNumber;
         final byte countOfRounds = 3;
         String[] questions = new String[countOfRounds];
         String[] rightAnswers = new String[countOfRounds];
         final int maxOperandSumDiff = 100;
-        final int maxOperandMult = 25;
+        int firstNumber;
+        int secondNumber;
 
         for (int i = 0; i < countOfRounds; i++) {
+            firstNumber = RandomUtils.nextInt(0, maxOperandSumDiff);
+            secondNumber = RandomUtils.nextInt(0, maxOperandSumDiff);
             switch (RandomUtils.nextInt(0, countOfRounds)) {
                 case 0:
-                    firstNumber = RandomUtils.nextInt(0, maxOperandSumDiff);
-                    secondNumber = RandomUtils.nextInt(0, maxOperandSumDiff);
-                    questions[i] = firstNumber + " + " + secondNumber;
-                    rightAnswers[i] = firstNumber + secondNumber + "";
+                    questions[i] = buildQuestion(" + ", firstNumber, secondNumber);
+                    rightAnswers[i] = buildRightAnswer(" + ", firstNumber, secondNumber);
                     break;
                 case 1:
-                    firstNumber = RandomUtils.nextInt(0, maxOperandSumDiff);
-                    secondNumber = RandomUtils.nextInt(0, maxOperandSumDiff);
-                    questions[i] = firstNumber + " - " + secondNumber;
-                    rightAnswers[i] = firstNumber - secondNumber + "";
+                    questions[i] = buildQuestion(" - ", firstNumber, secondNumber);
+                    rightAnswers[i] = buildRightAnswer(" - ", firstNumber, secondNumber);
                     break;
                 case 2:
-                    firstNumber = RandomUtils.nextInt(0, maxOperandMult);
-                    secondNumber = RandomUtils.nextInt(0, maxOperandMult);
-                    questions[i] = firstNumber + " * " + secondNumber;
-                    rightAnswers[i] = firstNumber * secondNumber + "";
+                    questions[i] = buildQuestion(" * ", firstNumber, secondNumber);
+                    rightAnswers[i] = buildRightAnswer(" * ", firstNumber, secondNumber);
                     break;
                 default:
                     break;
             }
         }
         Engine.gameLogic("What is the result of the expression?", questions, rightAnswers);
+    }
+
+    public static String buildQuestion(String operator, int firstNumber, int secondNumber) {
+        return firstNumber + operator + secondNumber;
+    }
+
+    public static String buildRightAnswer(String operator, int firstNumber, int secondNumber) {
+        return switch (operator) {
+            case " + " -> firstNumber + secondNumber + "";
+            case " - " -> firstNumber - secondNumber + "";
+            case " * " -> firstNumber * secondNumber + "";
+            default -> "Ops";
+        };
     }
 }
