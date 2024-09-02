407e431446515c9728bc32a36262079fb8b0e24f
KBA-a <mackh2121@gmail.com>
2024-09-02 21:54:51 +0500

    fix problem checking codeClimate

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -11,24 +11,32 @@ public class Prime {
         String[] rightAnswer = new String[countOfRounds];
         int number;
         final int maxNumber = 1000;
-        final int startNUmberSearching = 3;
+
 
         for (int i = 0; i < countOfRounds; i++) {
             number = RandomUtils.nextInt(2, maxNumber);
             question[i] = number + "";
-            rightAnswer[i] = "yes";
-            if (number % 2 == 0 && number != 2) {
-                rightAnswer[i] = "no";
-                continue;
-            }
-            for (int j = startNUmberSearching; j < number; j += 2) {
-                if (number % j == 0) {
-                    rightAnswer[i] = "no";
-                    break;
-                }
-            }
+            rightAnswer[i] = checkingForPrimeNUmber(number);
         }
         Engine.gameLogic("Answer 'yes' if given number is prime. Otherwise answer 'no'.",
                 question, rightAnswer);
+    }
+
+    public static String checkingForPrimeNUmber(int number) {
+
+        final int startNUmberSearching = 3;
+        String rightAnswer = "yes";
+
+        if (number % 2 == 0 && number != 2) {
+            rightAnswer = "no";
+            return rightAnswer;
+        }
+        for (int j = startNUmberSearching; j < number; j += 2) {
+            if (number % j == 0) {
+                rightAnswer = "no";
+                return rightAnswer;
+            }
+        }
+        return rightAnswer;
     }
 }
