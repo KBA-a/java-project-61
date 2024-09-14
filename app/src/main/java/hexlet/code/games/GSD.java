2bbb341eea3da05fa3594d1d5638f7b769040f0c
KBA-a <mackh2121@gmail.com>
2024-09-14 21:43:08 +0500

    fix GSD.java

--- app/src/main/java/hexlet/code/games/GSD.java
+++ app/src/main/java/hexlet/code/games/GSD.java
@@ -6,22 +6,22 @@ import org.apache.commons.lang3.RandomUtils;
 public class GSD {
     public static void gsd() {
 
-        String[] questions = new String[Engine.countOfRounds];
-        String[] rightAnswers = new String[Engine.countOfRounds];
+        String[] questions = new String[Engine.COUNT_OF_ROUNDS];
+        String[] rightAnswers = new String[Engine.COUNT_OF_ROUNDS];
         final int maxNumber = 100;
 
-        for (int i = 0; i < Engine.countOfRounds; i++) {
+        for (int i = 0; i < Engine.COUNT_OF_ROUNDS; i++) {
 
             int firstNumber = RandomUtils.nextInt(0, maxNumber);
             int secondNumber = RandomUtils.nextInt(0, maxNumber);
 
             questions[i] = firstNumber + " " + secondNumber;
-            rightAnswers[i] = searchingGSD(firstNumber, secondNumber);
+            rightAnswers[i] = searchingGSD(firstNumber, secondNumber) + "";
         }
         Engine.gameLogic("Find the greatest common divisor of given numbers.", questions, rightAnswers);
     }
 
-    public static String searchingGSD(int firstNumber, int secondNumber) {
+    public static int searchingGSD(int firstNumber, int secondNumber) {
         while (firstNumber != 0 && secondNumber != 0) {
             if (firstNumber > secondNumber) {
                 firstNumber = firstNumber % secondNumber;
@@ -29,6 +29,6 @@ public class GSD {
                 secondNumber = secondNumber % firstNumber;
             }
         }
-        return Math.max(firstNumber, secondNumber) + "";
+        return Math.max(firstNumber, secondNumber);
     }
 }
